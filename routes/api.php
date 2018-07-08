d77d26bdde901a3a576b2f61da9d424f1639e437
druos4 <druos4@gmail.com>
2018-07-08 22:44:25 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('comments', 'ArticleController@index');
 Route::get('comments/{article}', 'ArticleController@show');
 Route::post('comments', 'ArticleController@store');
 Route::put('comments/{article}', 'ArticleController@update');
